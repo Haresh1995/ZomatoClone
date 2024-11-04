1d5e3538377b15cf10be21081d883da07f0cd2dc
haresh <wala.harsh24@gmail.com>
2024-11-04 19:06:30 +0530

    Added admin requirements

--- ZomatoProj/zomatoapp/admin.py
+++ ZomatoProj/zomatoapp/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import User
+from .models import (
+    User,
+    Orders,
+    Restaurant,
+    DeliveryPerson,
+    Menu,
+    Rating,
+    Category
+)
 
 admin.site.register(User)
+admin.site.register(Orders)
+admin.site.register(Restaurant)
+admin.site.register(DeliveryPerson)
+admin.site.register(Rating)
+admin.site.register(Category)
