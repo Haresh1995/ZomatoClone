cf9d842bc5a1244afe66f129796eb3c1e04411b4
haresh <wala.harsh24@gmail.com>
2024-11-11 20:51:19 +0530

    Added serializer

--- ZomatoProj/constants.py
+++ ZomatoProj/constants.py
@@ -1,2 +1,4 @@
 USERS = "users/"
 USERS_BY_ID = "users/<str:user_id>"
+
+ORDER_ITEMS = "order_items/"
--- ZomatoProj/zomatoapp/serializers/orders.py
+++ ZomatoProj/zomatoapp/serializers/orders.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from ..models import Orders
+from ..models import Orders, OrderItem
 
 
 class OrdersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Orders
+        fields = "__all__"
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
         fields = "__all__"
