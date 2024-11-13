160cfec0ecb4801b0557b86f7ca4bc6927ebd163
haresh <wala.harsh24@gmail.com>
2024-11-13 18:34:34 +0530

    Added some changes in orders and categories

--- ZomatoProj/zomatoapp/models/category.py
+++ ZomatoProj/zomatoapp/models/category.py
@@ -11,17 +11,21 @@ class Category(BaseModel):
         default=uuid.uuid4,
         editable=False,
     )
-    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="categories")
     category_name = models.CharField(max_length=50)
+    is_available = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.category_name
+        return f"{self.category_name} - {self.menu.restaurant}"
 
 
 class FoodItem(BaseModel):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="food_items"
+    )
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=False)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
--- ZomatoProj/zomatoapp/urls/url.py
+++ ZomatoProj/zomatoapp/urls/url.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from ZomatoProj.constants import *
 from ..views.user import UserListView
-from ..views.orders import OrderItemsView
+
+# from ..views.orders import OrderItemsView
 
 urlpatterns = [
     path(USERS, UserListView.as_view()),
     path(USERS_BY_ID, UserListView.as_view()),
-    path(ORDER_ITEMS, OrderItemsView.as_view()),
+    # path(ORDER_ITEMS, OrderItemsView.as_view()),
 ]
